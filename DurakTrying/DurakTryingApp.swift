dc0f805da48c238e046009c90862eabb764626c6
sofya <nastya.skosyreva@gmail.com>
2024-11-14 16:38:22 +0000

    first drfat and loading screen

---
+++ DurakTrying/DurakTryingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DurakTryingApp.swift
+//  DurakTrying
+//
+//  Created by Sofya-Anastasia Skosyreva on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct DurakTryingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
